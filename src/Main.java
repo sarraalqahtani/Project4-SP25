d4d02662c52c436cdf761af110ff6064b8cfed14
alqahtas <alqahtas@wfu.edu>
2025-02-20 14:42:24 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static public void main(String args[]){
+        //To collect the execution time;
+        // use the following lines before and after calling the method
+        // you need to compute its execution tim
+        long startTime;
+        long finishTime;
+        startTime = System.nanoTime();
+        //method call
+        finishTime = System.nanoTime();
+
+    }
+}
